291386412a58da19d4deb560e188445c992c2176
Santiago Bermudez <santiago.baglietto@wolox.com.ar>
2013-09-27 20:30:26 -0300

    No est ainstrumentando el código.

---
+++ examples/singlylist/instrumented/SinglyLinkedList.java
@@ -0,0 +1,4 @@
+
+
+import java.io.IOException;
+import ar.edu.taco.utils.FileUtils;
---
+++ examples/singlylist/instrumented/ar/edu/itba/linedetector/SinglyLinkedList.java
@@ -0,0 +1,2 @@
+package examples.singlylist.instrumented.ar.edu.itba.linedetector;
+import ar.edu.taco.utils.FileUtils;
