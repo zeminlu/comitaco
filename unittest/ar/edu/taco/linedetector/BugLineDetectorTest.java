de9628af366d235efb4b9f4067c9a0f1b61b487e
ConcoMB <cmaderblanco@gmail.com>
2014-05-01 15:11:08 -0300

    marke

--- unittest/ar/edu/taco/linedetector/BugLineDetectorTest.java
+++ unittest/ar/edu/taco/linedetector/BugLineDetectorTest.java
@@ -8,21 +8,27 @@ import ar.uba.dc.rfm.dynalloy.visualization.VizException;
 public class BugLineDetectorTest extends CollectionTestBase {
 
 	private static String testClassPath = "roops.core.objects";
-	//private static String testClassPath = "examples.singlylist";
+	// private static String testClassPath = "examples.singlylist";
 
 	@Override
 	protected String getClassToCheck() {
-		return testClassPath+".SinglyLinkedList";
+		return testClassPath + ".SinglyLinkedList";
 	}
 
 	public void test_contains() throws VizException {
-		setConfigKeyRelevantClasses(testClassPath+".SinglyLinkedList,"+testClassPath+".SinglyLinkedListNode");
+		setConfigKeyRelevantClasses(testClassPath + ".SinglyLinkedList,"
+				+ testClassPath + ".SinglyLinkedListNode," + testClassPath
+				+ ".BugLineMarker");
 		setConfigKeyRelevancyAnalysis(true);
 		setConfigKeyCheckNullDereference(true);
 		setConfigKeyUseJavaArithmetic(false);
 		setConfigKeyObjectScope(3);
 		setConfigKeyInferScope(false);
-		setConfigKeyTypeScopes("examples.singlylist.SinglyLinkedList:1,examples.singlylist.SinglyLinkedListNode:7");
+
+		setConfigKeyTypeScopes(testClassPath + ".SinglyLinkedList:1,"
+				+ testClassPath + ".SinglyLinkedListNode:2");
+		// setConfigKeyTypeScopes("examples.singlylist.SinglyLinkedList:1,examples.singlylist.SinglyLinkedListNode:7");
+
 		setConfigKeySkolemizeInstanceInvariant(true);
 		setConfigKeySkolemizeInstanceAbstraction(true);
 		setConfigKeyGenerateUnitTestCase(false);
@@ -32,10 +38,14 @@ public class BugLineDetectorTest extends CollectionTestBase {
 		newOverProp.put("generateRun", "false");
 		newOverProp.put("include_simulation_program_declaration", "true");
 
-		BugLineDetector main = new BugLineDetector(GENERIC_PROPERTIES, newOverProp, "contains_0");
+		BugLineDetector main = new BugLineDetector(GENERIC_PROPERTIES,
+				newOverProp, "contains_0");
 		System.out.println("Entrando al run...");
 
-		main.run("roops/core/objects/SinglyLinkedList.java"/*"examples/singlylist/SinglyLinkedList.java"*/);
+		main.run("roops/core/objects/SinglyLinkedList.java"/* "examples/singlylist/SinglyLinkedList.java" */);
+
+		// main.run(System.getProperty("user.dir") +
+		// "/tests/examples/singlylist/SinglyLinkedList.java");
 		System.out.println("Salido del run.");
 	}
 }
