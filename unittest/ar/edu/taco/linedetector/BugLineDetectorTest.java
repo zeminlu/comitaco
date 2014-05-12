5d4742f19970b2b1b860c38f9311c48892807d31
ConcoMB <cmaderblanco@gmail.com>
2014-05-12 11:05:08 -0300

    markmaker

--- unittest/ar/edu/taco/linedetector/BugLineDetectorTest.java
+++ unittest/ar/edu/taco/linedetector/BugLineDetectorTest.java
@@ -1,6 +1,11 @@
 package ar.edu.taco.linedetector;
 
+import java.io.IOException;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Map.Entry;
+
+import edu.mit.csail.sdg.alloy4.Pair;
 
 import ar.edu.taco.regresion.CollectionTestBase;
 import ar.uba.dc.rfm.dynalloy.visualization.VizException;
@@ -40,12 +45,40 @@ public class BugLineDetectorTest extends CollectionTestBase {
 
 		BugLineDetector main = new BugLineDetector(GENERIC_PROPERTIES,
 				newOverProp, "contains_0");
+		System.out.println("Agregando marcas");
+		
+		MarkMaker mm = new MarkMaker("/Users/concoMB/pf/comitaco/tests/roops/core/objects/SinglyLinkedList.java", "contains");
+		try {
+			mm.mark();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
 		System.out.println("Entrando al run...");
 
+		
 		main.run("roops/core/objects/SinglyLinkedList.java"/* "examples/singlylist/SinglyLinkedList.java" */);
 
 		// main.run(System.getProperty("user.dir") +
 		// "/tests/examples/singlylist/SinglyLinkedList.java");
 		System.out.println("Salido del run.");
+		
+		MarkParser mp = new MarkParser("/Users/concoMB/pf/comitaco/output/output.als");
+		MarkCleaner mc = new MarkCleaner("/Users/concoMB/pf/comitaco/tests/roops/core/objects/SinglyLinkedList.java");
+		try {
+			Map<Integer, Pair<Integer, Integer>> m = mp.parse();
+			for (Entry<Integer, Pair<Integer, Integer>> e : m.entrySet()) {
+				System.out.println(e.getKey() + ":");
+				System.out.println("\t " + e.getValue().a + " - "
+						+ e.getValue().b);
+			}
+			System.out.println(mp.getOriginalLine(3));
+			System.out.println(mp.getOriginalLine(30));
+			System.out.println("cleaning up");
+			mc.clean();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
