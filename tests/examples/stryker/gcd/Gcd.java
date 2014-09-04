87d1f2ee821c75d5a68b97ff3bca8d480177f6c7
Marcelo Frias <mffrias@gmail.com>
2014-09-04 06:43:17 -0300

    gcd example that genprog is unable to repair.

--- tests/examples/stryker/gcd/Gcd.java
+++ tests/examples/stryker/gcd/Gcd.java
@@ -1,30 +1,38 @@
+// This is mutant program.
+// Author : ysma
+
 package examples.stryker.gcd;
 
+
 public class Gcd {
-	
+
+	public Gcd ()
+	{
+	}
+
 	/*@
 	@ requires aa >= 0 && bb >= 0;
 	@ ensures \result >= 0;
 	@ ensures (aa % \result == 0); 
-	@ ensures (bb % \result == 0 || bb == \result); 
+	@ ensures (bb % \result == 0); 
 	@ signals (Exception e) false; 
-	@*/
-	public static int gcd(final int aa, final int bb) {
-      int a = aa;
-      int b = bb;
-	  if (a == 0) {
-		  return b;
-	  } 
-	  else
-	  {
-	    while (b != 0) {
-	      if (a > b) {
-	        a = a - b; 
-	      } else {
-	        b = b - a; 
-	      } 
-	    } 
-	    return a;
-	  } 
-	} 	
+	@*/    
+	public int gcd ( final int aa, final int bb)
+	{
+
+		int a = aa;
+		int b = bb;
+		if ( a == 0 ) {
+			return --b; //mutGenLimit 1
+		} else {
+			while ( b != 0 ) {
+				if ( a <= b ) {
+					a = a - b; 
+				} else {
+					b = b - a;
+				} //mutGenLimit 1
+			}
+			return a;
+		}
+	}
 }
