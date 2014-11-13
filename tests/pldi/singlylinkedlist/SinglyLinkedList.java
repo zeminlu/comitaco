3d4d5f7bde033cf4a48948dca003eb234b6c2136
Marcelo Frias <mffrias@gmail.com>
2014-11-13 18:25:25 -0300

    Fixed contract for contains

--- tests/pldi/singlylinkedlist/SinglyLinkedList.java
+++ tests/pldi/singlylinkedlist/SinglyLinkedList.java
@@ -6,88 +6,107 @@ import pldi.singlylinkedlist.SinglyLinkedListNode;
 
 public class SinglyLinkedList {
 
-	/*@
+/*@
     @ invariant (\forall SinglyLinkedListNode n; \reach(this.header, SinglyLinkedListNode, next).has(n); \reach(n.next, SinglyLinkedListNode, next).has(n)==false);
-    @*/    
+    @*/    public /*@nullable@*/pldi.singlylinkedlist.SinglyLinkedListNode header;
 
-	public /*@nullable@*/SinglyLinkedListNode header;
+    public SinglyLinkedList () {
+    }
 
-	public SinglyLinkedList() {
-	}
-
-	/*@ 
+/*@ 
       @ requires true;
-      @ ensures (\exists SinglyLinkedListNode n; \reach(this.header, SinglyLinkedListNode, next).has(n); n.value==valueParam) ==> (\result==true);
-      @ ensures (\result == true) ==> (\exists SinglyLinkedListNode n; \reach(this.header, SinglyLinkedListNode, next).has(n); n.value==valueParam);
+      @ ensures (\exists SinglyLinkedListNode n; \old(\reach(this.header, SinglyLinkedListNode, next)).has(n); n.\old(value)==valueParam) ==> (\result==true);
+      @ ensures (\result == true) ==> (\exists SinglyLinkedListNode n; \old(\reach(this.header, SinglyLinkedListNode, next).has(n)); n.\old(value)==valueParam);
       @ signals (RuntimeException e) false;
       @ 
-      @*/    public boolean contains( /*@nullable@*/java.lang.Object valueParam ) {
-    	  SinglyLinkedListNode current;
-    	  boolean result;
-    	  current = this.header.next; //mutGenLimit 1
-    	  result = false;
-    	  while (result == false && current != null) {
-    		  boolean equalVal;
-    		  if (valueParam == null && current.value == null) {
-    			  equalVal = false; //mutGenLimit 1
-    		  } else {
-    			  if (valueParam == null) { //mutGenLimit 1
-    				  if (valueParam == current) { //mutGenLimit 1
-    					  equalVal = true;
-    				  } else {
-    					  equalVal = false;
-    				  }
-    			  } else {
-    				  equalVal = false;
-    			  }
-    		  }
-    		  if (equalVal == true) {
-    			  result = true;
-    		  }
-    		  current.next = current.next.next; //mutGenLimit 2
-    	  }
-    	  return result;
-      }
+      @*/    public boolean contains ( /*@nullable@*/java.lang.Object valueParam) {
+        pldi.singlylinkedlist.SinglyLinkedListNode current;
+        boolean result;
+        current = this.header;
+        result = false;
+        while ( result == false && current != null ) {
+            boolean equalVal;
+            if ( valueParam == null && current.value == null ) {
+                equalVal = true;
+            } else {
+                if ( valueParam != null ) {
+                    if ( valueParam == current ) { //mutGenLimit 1
+                        equalVal = true;
+                    } else {
+                        equalVal = false;
+                    }
+                } else {
+                    equalVal = false;
+                }
+            }
+            if ( equalVal == true ) {
+                result = true;
+            }
+            current = current.next;
+        }
+        return result;
+    }
 
-      /*@
+/*@
       @ requires index>=0 && index<\reach(this.header, SinglyLinkedListNode, next).int_size();
       @
       @ ensures \reach(this.header, SinglyLinkedListNode, next).has(\result)==true;
       @ ensures \reach(\result, SinglyLinkedListNode, next).int_size() == \reach(this.header, SinglyLinkedListNode, next).int_size()-index;
       @ signals (RuntimeException e) false;
-      @*/    
+      @*/    public pldi.singlylinkedlist.SinglyLinkedListNode getNode ( int index, boolean customvar_0, boolean customvar_1, boolean customvar_2, boolean customvar_3) {
+        pldi.singlylinkedlist.SinglyLinkedListNode current = this.header;
+        pldi.singlylinkedlist.SinglyLinkedListNode result = null;
+        int current_index = 0;
+        {
+            if ( result == null && ! (current == null) ) { //mutGenLimit 0 mutID 1
+                if ( customvar_0 ) { //mutGenLimit 0 mutID 2
+                    result = current;
+                }
+                index = current_index * 1;
+                current = current.next;
+            }
+            if ( result == null && ! (current == null) ) { //mutGenLimit 0 mutID 1
+                if ( customvar_1 ) { //mutGenLimit 0 mutID 2
+                    result = current;
+                }
+                index = current_index * 1;
+                current = current.next;
+            }
+            if ( result == null && ! (current == null) ) { //mutGenLimit 0 mutID 1
+                if ( customvar_2 ) { //mutGenLimit 0 mutID 2
+                    result = current;
+                }
+                index = current_index * 1;
+                current = current.next;
+            }
+            if ( result == null && ! (current == null) ) { //mutGenLimit 0 mutID 1
+                if ( customvar_3 ) { //mutGenLimit 0 mutID 2
+                    result = current;
+                }
+                index = current_index * 1;
+                current = current.next;
+            }
+        }
+        return result;
+    }
 
-      public SinglyLinkedListNode getNode( int index ) {
-    	  SinglyLinkedListNode current = this.header;
-    	  SinglyLinkedListNode result = null;
-    	  int current_index = 0;
-    	  while (result == null && current == null) { //mutGenLimit 1
-    		  if (index < current_index) { //mutGenLimit 1
-    			  current.next = current; //mutGenLimit 1
-    		  }
-    		  current_index = current_index * 1; //mutGenLimit 1
-    		  current.next = current.next; //mutGenLimit 1
-    	  }
-    	  return result;   
-      }
-
-      /*@ requires true;
+/*@ requires true;
       @ ensures (\exists SinglyLinkedListNode n; \reach(this.header, SinglyLinkedListNode, next).has(n); n.value == arg && n.next == null);
       @ ensures (\forall SinglyLinkedListNode n; \reach(this.header, SinglyLinkedListNode, next).has(n); n.next != null ==> \old(\reach(this.header, SinglyLinkedListNode, next)).has(n));
-      @*/    public void insertBack( java.lang.Object arg ) {
-    	  SinglyLinkedListNode freshNode = new SinglyLinkedListNode();
-    	  freshNode.value = arg;
-    	  freshNode.next = null;
-    	  if (this.header == null) {
-    		  this.header = freshNode;
-    	  } else {
-    		  SinglyLinkedListNode current;
-    		  current = this.header;
-    		  while (current.next != null) {
-    			  current = current.next;
-    		  }
-    		  current.next = freshNode;
-    	  }
-      }
-
+      @ ensures (\forall SinglyLinkedListNode n; \old(\reach(this.header, SinglyLinkedListNode, next)).has(n); \reach(this.header, SinglyLinkedListNode, next).has(n) && n.next != null);
+      @*/    public void insertBack ( java.lang.Object arg) {
+        pldi.singlylinkedlist.SinglyLinkedListNode freshNode = new pldi.singlylinkedlist.SinglyLinkedListNode ();
+        freshNode.value = arg;
+        freshNode.next = null;
+        if ( this.header == null ) {
+            this.header = freshNode;
+        } else {
+            pldi.singlylinkedlist.SinglyLinkedListNode current;
+            current = this.header;
+            while ( current.next != null ) {
+                current = current.next;
+            }
+            current.next = freshNode;
+        }
+    }
 }
