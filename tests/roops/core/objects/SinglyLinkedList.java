3ced432a2d222cfaae15b75f29b0ab548a41268b
ConcoMB <cmaderblanco@gmail.com>
2014-11-21 16:41:57 -0300

    ep
    Conflicts:
    	bin/roops/core/objects/sequential/SinglyLinkedList.java_contains
    	tests/roops/core/objects/SinglyLinkedList.java

--- tests/roops/core/objects/SinglyLinkedList.java
+++ tests/roops/core/objects/SinglyLinkedList.java
@@ -28,7 +28,6 @@ public class SinglyLinkedList {
     }
 
 //-------------------- contains -------------------------//
-
     /*@
         @ requires value_param != null;
         @ ensures (\exists SinglyLinkedListNode n; \reach(this.header, SinglyLinkedListNode, next).has(n); n.value==value_param)
@@ -36,7 +35,6 @@ public class SinglyLinkedList {
         @ signals (RuntimeException e) false;
         @*/
     public boolean contains (  /*@nullable@*/ java.lang.Object value_param) {
-        fajita_roopsGoal_initialization ();
         roops.core.objects.SinglyLinkedListNode current;
         BugLineMarker dummy;
         boolean result;
@@ -45,50 +43,199 @@ public class SinglyLinkedList {
             = this.header;
         result = false;
         {
-            boolean fajita_cicle_0 = false;
-
-            while ( result == false && current != null ) {
-                fajita_cicle_0 = true;
-                roops_goal_0 = true;
+            boolean terminatesInTime = false;
+            if ( result == false && current != null ) {
                 boolean equalVal;
                 if ( value_param != null && current.value == null ) {
-                    roops_goal_2 = true;
                     // equalVal = false; //mutGenLimit 1
-                    equalVal
-                        = true;
+                    equalVal = true;
                 } else {
-                    roops_goal_3 = true;
                     if ( value_param != null ) {
-                        roops_goal_4 = true;
                         if ( value_param == current.value ) {
-                            roops_goal_6 = true;
                             equalVal = true;
                         } else {
-                            roops_goal_7 = true;
                             equalVal = false;
                         }
                     } else {
-                        roops_goal_5 = true;
                         equalVal = false;
                     }
                 }
                 if ( equalVal == true ) {
-                    roops_goal_8 = true;
                     result = false;
-                } else {
-                    roops_goal_9 = true;
                 }
                 // current = current.next.next; //mutGenLimit 1
                 current
                     = current.next;
             }
-            if ( ! fajita_cicle_0 ) {
-                roops_goal_1 = true;
+            if ( result == false && current != null ) {
+                boolean equalVal;
+                if ( value_param != null && current.value == null ) {
+                    // equalVal = false; //mutGenLimit 1
+                    equalVal = true;
+                } else {
+                    if ( value_param != null ) {
+                        if ( value_param == current.value ) {
+                            equalVal = true;
+                        } else {
+                            equalVal = false;
+                        }
+                    } else {
+                        equalVal = false;
+                    }
+                }
+                if ( equalVal == true ) {
+                    result = false;
+                }
+                // current = current.next.next; //mutGenLimit 1
+                current
+                    = current.next;
+            }
+            if ( result == false && current != null ) {
+                boolean equalVal;
+                if ( value_param != null && current.value == null ) {
+                    // equalVal = false; //mutGenLimit 1
+                    equalVal = true;
+                } else {
+                    if ( value_param != null ) {
+                        if ( value_param == current.value ) {
+                            equalVal = true;
+                        } else {
+                            equalVal = false;
+                        }
+                    } else {
+                        equalVal = false;
+                    }
+                }
+                if ( equalVal == true ) {
+                    result = false;
+                }
+                // current = current.next.next; //mutGenLimit 1
+                current
+                    = current.next;
+            }
+            if ( result == false && current != null ) {
+                boolean equalVal;
+                if ( value_param != null && current.value == null ) {
+                    // equalVal = false; //mutGenLimit 1
+                    equalVal = true;
+                } else {
+                    if ( value_param != null ) {
+                        if ( value_param == current.value ) {
+                            equalVal = true;
+                        } else {
+                            equalVal = false;
+                        }
+                    } else {
+                        equalVal = false;
+                    }
+                }
+                if ( equalVal == true ) {
+                    result = false;
+                }
+                // current = current.next.next; //mutGenLimit 1
+                current
+                    = current.next;
+            }
+            if ( result == false && current != null ) {
+                boolean equalVal;
+                if ( value_param != null && current.value == null ) {
+                    // equalVal = false; //mutGenLimit 1
+                    equalVal = true;
+                } else {
+                    if ( value_param != null ) {
+                        if ( value_param == current.value ) {
+                            equalVal = true;
+                        } else {
+                            equalVal = false;
+                        }
+                    } else {
+                        equalVal = false;
+                    }
+                }
+                if ( equalVal == true ) {
+                    result = false;
+                }
+                // current = current.next.next; //mutGenLimit 1
+                current
+                    = current.next;
+            }
+            if ( result == false && current != null ) {
+                boolean equalVal;
+                if ( value_param != null && current.value == null ) {
+                    // equalVal = false; //mutGenLimit 1
+                    equalVal = true;
+                } else {
+                    if ( value_param != null ) {
+                        if ( value_param == current.value ) {
+                            equalVal = true;
+                        } else {
+                            equalVal = false;
+                        }
+                    } else {
+                        equalVal = false;
+                    }
+                }
+                if ( equalVal == true ) {
+                    result = false;
+                }
+                // current = current.next.next; //mutGenLimit 1
+                current
+                    = current.next;
+            }
+            if ( result == false && current != null ) {
+                boolean equalVal;
+                if ( value_param != null && current.value == null ) {
+                    // equalVal = false; //mutGenLimit 1
+                    equalVal = true;
+                } else {
+                    if ( value_param != null ) {
+                        if ( value_param == current.value ) {
+                            equalVal = true;
+                        } else {
+                            equalVal = false;
+                        }
+                    } else {
+                        equalVal = false;
+                    }
+                }
+                if ( equalVal == true ) {
+                    result = false;
+                }
+                // current = current.next.next; //mutGenLimit 1
+                current
+                    = current.next;
+            }
+            if ( result == false && current != null ) {
+                boolean equalVal;
+                if ( value_param != null && current.value == null ) {
+                    // equalVal = false; //mutGenLimit 1
+                    equalVal = true;
+                } else {
+                    if ( value_param != null ) {
+                        if ( value_param == current.value ) {
+                            equalVal = true;
+                        } else {
+                            equalVal = false;
+                        }
+                    } else {
+                        equalVal = false;
+                    }
+                }
+                if ( equalVal == true ) {
+                    result = false;
+                }
+                // current = current.next.next; //mutGenLimit 1
+                current
+                    = current.next;
+            }
+            if ( result == false && current != null ) {
+                terminatesInTime = true;
             }
         }
-        // return !result; //mutGenLimit 1
         return result;
     }
+
+//--------------------------- getNode ----------------------------//
     /*@
         @ requires index>=0 && index<\reach(this.header, SinglyLinkedListNode, next).int_size();
         @ ensures \reach(this.header, SinglyLinkedListNode, next).has(\result)==true;
@@ -99,13 +246,67 @@ public class SinglyLinkedList {
         roops.core.objects.SinglyLinkedListNode current = header;
         roops.core.objects.SinglyLinkedListNode result = null;
         int current_index = 0;
-        while ( result == null && current != null ) {
-            if ( index == current_index ) {
-                result = current;
-            } else {
+        {
+            boolean terminatesInTime = false;
+            if ( result == null && current != null ) {
+                if ( index == current_index ) {
+                    result = current;
+                }
+                current_index = current_index + 1;
+                current = current.next;
             }
-            current_index = current_index + 1;
-            current = current.next;
+            if ( result == null && current != null ) {
+                if ( index == current_index ) {
+                    result = current;
+                }
+                current_index = current_index + 1;
+                current = current.next;
+            }
+            if ( result == null && current != null ) {
+                if ( index == current_index ) {
+                    result = current;
+                }
+                current_index = current_index + 1;
+                current = current.next;
+            }
+            if ( result == null && current != null ) {
+                if ( index == current_index ) {
+                    result = current;
+                }
+                current_index = current_index + 1;
+                current = current.next;
+            }
+            if ( result == null && current != null ) {
+                if ( index == current_index ) {
+                    result = current;
+                }
+                current_index = current_index + 1;
+                current = current.next;
+            }
+            if ( result == null && current != null ) {
+                if ( index == current_index ) {
+                    result = current;
+                }
+                current_index = current_index + 1;
+                current = current.next;
+            }
+            if ( result == null && current != null ) {
+                if ( index == current_index ) {
+                    result = current;
+                }
+                current_index = current_index + 1;
+                current = current.next;
+            }
+            if ( result == null && current != null ) {
+                if ( index == current_index ) {
+                    result = current;
+                }
+                current_index = current_index + 1;
+                current = current.next;
+            }
+            if ( result == null && current != null ) {
+                terminatesInTime = true;
+            }
         }
         return result;
     }
@@ -132,43 +333,37 @@ public class SinglyLinkedList {
             this.header = freshNode;
         } else {
             roops.core.objects.SinglyLinkedListNode current = this.header;
-            while ( current.next != null ) {
-                current = current.next;
+            {
+                boolean terminatesInTime = false;
+                if ( current.next != null ) {
+                    current = current.next;
+                }
+                if ( current.next != null ) {
+                    current = current.next;
+                }
+                if ( current.next != null ) {
+                    current = current.next;
+                }
+                if ( current.next != null ) {
+                    current = current.next;
+                }
+                if ( current.next != null ) {
+                    current = current.next;
+                }
+                if ( current.next != null ) {
+                    current = current.next;
+                }
+                if ( current.next != null ) {
+                    current = current.next;
+                }
+                if ( current.next != null ) {
+                    current = current.next;
+                }
+                if ( current.next != null ) {
+                    terminatesInTime = true;
+                }
             }
             current.next = freshNode;
         }
-    }
-
-    public static boolean roops_goal_0;
-
-    public static boolean roops_goal_1;
-
-    public static boolean roops_goal_2;
-
-    public static boolean roops_goal_3;
-
-    public static boolean roops_goal_4;
-
-    public static boolean roops_goal_5;
-
-    public static boolean roops_goal_6;
-
-    public static boolean roops_goal_7;
-
-    public static boolean roops_goal_8;
-
-    public static boolean roops_goal_9;
-
-    public static void fajita_roopsGoal_initialization () {
-        roops_goal_0 = false;
-        roops_goal_1 = false;
-        roops_goal_2 = false;
-        roops_goal_3 = false;
-        roops_goal_4 = false;
-        roops_goal_5 = false;
-        roops_goal_6 = false;
-        roops_goal_7 = false;
-        roops_goal_8 = false;
-        roops_goal_9 = false;
     }
 }
