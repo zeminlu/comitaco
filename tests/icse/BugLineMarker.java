6038932fced824d0d85b4174ef048928f34f528e
Santiago Bermudez <santiago.baglietto@wolox.com.ar>
2015-08-04 18:55:20 -0300

    BinomialHeap ICSE BugLineDetector Experiments

---
+++ tests/icse/BugLineMarker.java
@@ -0,0 +1,19 @@
+package icse;
+
+/*@nullable_by_default@*/
+public class BugLineMarker { 
+	public BugLineMarker() {}	
+	public void mark(int l) {} 
+public void mark58() {}
+public void mark59() {}
+public void mark60() {}
+public void mark61() {}
+public void mark62() {}
+public void mark63() {}
+public void mark64() {}
+public void mark65() {}
+public void mark66() {}
+public void mark68() {}
+public void mark69() {}
+public void mark70() {}
+}
