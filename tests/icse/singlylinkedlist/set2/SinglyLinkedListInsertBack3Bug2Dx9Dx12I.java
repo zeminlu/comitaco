fe0bde2daec7adaa2e59621745b1325eeccea824
Marcelo Frias <mffrias@gmail.com>
2015-08-25 01:12:42 +0200

    Modified tests/icse/singlylinkedlist/set2/SinglyLinkedListInsertBack3Bug2Dx9Dx12I.java so that it compiles`

--- tests/icse/singlylinkedlist/set2/SinglyLinkedListInsertBack3Bug2Dx9Dx12I.java
+++ tests/icse/singlylinkedlist/set2/SinglyLinkedListInsertBack3Bug2Dx9Dx12I.java
@@ -5,106 +5,96 @@ import icse.singlylinkedlist.SinglyLinkedListNode;
 
 
 /**
-* SinglyLinkedListInsertBackBug9x12Ix8I
-*
-*/
+ * SinglyLinkedListInsertBackBug9x12Ix8I
+ *
+ */
 public class SinglyLinkedListInsertBack3Bug2Dx9Dx12I {
 
-    /*@
+	/*@
     @ invariant (\forall SinglyLinkedListNode n; \reach(this.header, SinglyLinkedListNode, next).has(n); \reach(n.next, SinglyLinkedListNode, next).has(n)==false);
     @*/
-    public /*@nullable@*/icse.singlylinkedlist.SinglyLinkedListNode header;
+	public /*@nullable@*/icse.singlylinkedlist.SinglyLinkedListNode header;
 
-    public SinglyLinkedListInsertBack3Bug2Dx9Dx12I () {
-    }
+	public SinglyLinkedListInsertBack3Bug2Dx9Dx12I () {
+	}
 
-    /*@
+	/*@
     @ requires true;
     @ ensures (\exists SinglyLinkedListNode n; \reach(this.header, SinglyLinkedListNode, next).has(n); n.value==valueParam) ==> (\result==true);
     @ ensures (\result == true) ==> (\exists SinglyLinkedListNode n; \reach(this.header, SinglyLinkedListNode, next).has(n); n.value==valueParam);
     @ signals (RuntimeException e) false;
     @
     @*/
-    public boolean contains ( /*@nullable@*/java.lang.Object valueParam) {
-        icse.singlylinkedlist.SinglyLinkedListNode current;
-        boolean result;
-        current = this.header;
-        result = false;
-        while ( result == false && current != null ) {
-            boolean equalVal;
-            if ( valueParam == null && current.value == null ) {
-                equalVal = true;
-            } else {
-                if ( valueParam != null ) {
-                    if ( valueParam == current.value ) {
-                        equalVal = true;
-                    } else {
-                        equalVal = false;
-                    }
-                } else {
-                    equalVal = false;
-                }
-            }
-            if ( equalVal == true ) {
-                result = true;
-            }
-            current = current.next;
-        }
-        return result;
-    }
+	public boolean contains ( /*@nullable@*/java.lang.Object valueParam) {
+		icse.singlylinkedlist.SinglyLinkedListNode current;
+		boolean result;
+		current = this.header;
+		result = false;
+		while ( result == false && current != null ) {
+			boolean equalVal;
+			if ( valueParam == null && current.value == null ) {
+				equalVal = true;
+			} else {
+				if ( valueParam != null ) {
+					if ( valueParam == current.value ) {
+						equalVal = true;
+					} else {
+						equalVal = false;
+					}
+				} else {
+					equalVal = false;
+				}
+			}
+			if ( equalVal == true ) {
+				result = true;
+			}
+			current = current.next;
+		}
+		return result;
+	}
 
-    /*@
+	/*@
     @ requires index>=0 && index<\reach(this.header, SinglyLinkedListNode, next).int_size();
     @
     @ ensures \reach(this.header, SinglyLinkedListNode, next).has(\result)==true;
     @ ensures \reach(\result, SinglyLinkedListNode, next).int_size() == \reach(this.header, SinglyLinkedListNode, next).int_size()-index;
     @ signals (RuntimeException e) false;
     @*/
-    public icse.singlylinkedlist.SinglyLinkedListNode getNode ( int index) {
-        icse.singlylinkedlist.SinglyLinkedListNode current = this.header;
-        icse.singlylinkedlist.SinglyLinkedListNode result = null;
-        int current_index = 0;
-        while ( result == null && current != null ) {
-            if ( index == current_index ) {
-                result = current;
-            }
-            current_index = current_index + 1;
-            current = current.next;
-        }
-        return result;
-    }
+	public icse.singlylinkedlist.SinglyLinkedListNode getNode ( int index) {
+		icse.singlylinkedlist.SinglyLinkedListNode current = this.header;
+		icse.singlylinkedlist.SinglyLinkedListNode result = null;
+		int current_index = 0;
+		while ( result == null && current != null ) {
+			if ( index == current_index ) {
+				result = current;
+			}
+			current_index = current_index + 1;
+			current = current.next;
+		}
+		return result;
+	}
 
-    /*@ requires true;
+    /*@ requires freshNode != null;
+    @ requires freshNode.next == null;
+    @ requires \reach(header, SinglyLinkedListNode, next).has(freshNode) == false;
     @ ensures (\exists SinglyLinkedListNode n; \reach(this.header, SinglyLinkedListNode, next).has(n); n.value == arg && n.next == null);
     @ ensures (\forall SinglyLinkedListNode n; \reach(this.header, SinglyLinkedListNode, next).has(n); n.next != null ==> \old(\reach(this.header, SinglyLinkedListNode, next)).has(n));
     @ ensures (\forall SinglyLinkedListNode n; \old(\reach(this.header, SinglyLinkedListNode, next)).has(n); \reach(this.header, SinglyLinkedListNode, next).has(n) && n.next != null);
     @ signals (Exception e) false;
     @*/
-    public void insertBack ( java.lang.Object arg, boolean customvar_0, boolean customvar_1, boolean customvar_2, boolean customvar_3) {
-        icse.singlylinkedlist.SinglyLinkedListNode freshNode = new icse.singlylinkedlist.SinglyLinkedListNode ();
-        freshNode.value = new java.lang.Integer ( 1); //mutGenLimit 0 mutID 1
-        freshNode
-        .next = null;
-        if ( this.header == null ) {
-            this.header = freshNode;
-        } else {
-            icse.singlylinkedlist.SinglyLinkedListNode current;
-            current = this.header;
-            {
-                if ( customvar_0 ) { //mutGenLimit 1 mutID 2
-                    current = current.next;
-                }
-                if ( customvar_1 ) { //mutGenLimit 1 mutID 2
-                    current = current.next;
-                }
-                if ( customvar_2 ) { //mutGenLimit 1 mutID 2
-                    current = current.next;
-                }
-                if ( customvar_3 ) { //mutGenLimit 1 mutID 2
-                    current = current.next;
-                }
-            }
-            current = freshNode;
-        } //mutGenLimit 1 mutID 3
-    }
+    public void insertBack( java.lang.Object arg,  SinglyLinkedListNode freshNode) {
+		freshNode.value = null; //mutGenLimit 1
+		freshNode.next = null; //mutGenLimit 0
+		if (this.header == null) { //mutGenLimit 0
+			this.header = freshNode; //mutGenLimit 0
+		} else {
+			icse.singlylinkedlist.SinglyLinkedListNode current; //mutGenLimit 0
+			current = this.header; //mutGenLimit 0
+			//@decreasing \reach(current, SinglyLinkedListNode, next).int_size();
+			while (current.next != this.header) { //mutGenLimit 1
+				current = current.next; //mutGenLimit 0            
+			}
+			current = freshNode; //mutGenLimit 1
+		}
+	}
 }
