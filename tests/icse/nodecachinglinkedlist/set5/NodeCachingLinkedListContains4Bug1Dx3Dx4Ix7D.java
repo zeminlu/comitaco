e0f1e4ef6688fb0b5cee54d9ad646564cb68218d
Marcelo Frias <mffrias@gmail.com>
2015-08-24 22:51:20 +0200

    Modified tests/icse/nodecachinglinkedlist/set5/NodeCachingLinkedListContains4Bug1Dx3Dx4Ix7D.java so tha it compiles by removing /*@pure@*/

--- tests/icse/nodecachinglinkedlist/set5/NodeCachingLinkedListContains4Bug1Dx3Dx4Ix7D.java
+++ tests/icse/nodecachinglinkedlist/set5/NodeCachingLinkedListContains4Bug1Dx3Dx4Ix7D.java
@@ -145,7 +145,7 @@ public class NodeCachingLinkedListContains4Bug1Dx3Dx4Ix7D {
     @ ensures (\forall LinkedListNode n; \reach(header, LinkedListNode, next).has(n); \old(\reach(header, LinkedListNode, next)).has(n));
     @ signals (Exception e) false;
     @*/
-    public /*@ pure @*/boolean contains( /*@ nullable @*/java.lang.Object arg ) {
+    public boolean contains( /*@ nullable @*/java.lang.Object arg ) {
         icse.nodecachinglinkedlist.LinkedListNode node = this.header.next; //mutGenLimit 1
         int counter = 0; //mutGenLimit 0
         //@decreasing size - counter;
