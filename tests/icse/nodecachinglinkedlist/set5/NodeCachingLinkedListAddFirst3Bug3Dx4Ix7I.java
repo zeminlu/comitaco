3135df0c07b251c9261db09bb594e3a9756fcd2d
Marcelo Frias <mffrias@gmail.com>
2015-08-27 20:22:39 +0200

    Modified specs so that faxes are found

--- tests/icse/nodecachinglinkedlist/set5/NodeCachingLinkedListAddFirst3Bug3Dx4Ix7I.java
+++ tests/icse/nodecachinglinkedlist/set5/NodeCachingLinkedListAddFirst3Bug3Dx4Ix7I.java
@@ -117,16 +117,21 @@ public class NodeCachingLinkedListAddFirst3Bug3Dx4Ix7I {
         return oldValue; //mutGenLimit 0
     }
 
-    /*@ requires true;
+    /*@ requires newNode != null;
+    @ requires newNode.next == null;
+    @ requires newNode.previous == null;
+    @ requires newNode.value == null;
+    @ requires \reach(header, LinkedListNode, next).has(newNode) == false;
+    @ requires \reach(firstCachedNode, LinkedListNode, next).has(newNode) == false;
     @ ensures size == \old(size) + 1;
     @ ensures modCount == \old(modCount) + 1;
     @ ensures ( \forall LinkedListNode n; \old(\reach(header, LinkedListNode, next)).has(n); \reach(header, LinkedListNode, next).has(n));
     @ ensures ( \forall LinkedListNode n; \reach(header, LinkedListNode, next).has(n) && n != header.next; \old(\reach(header, LinkedListNode, next)).has(n) );
     @ ensures ( header.next.value == o );
     @ ensures \result == true;
+    @ signals (Exception e) false;
     @*/
-    public boolean addFirst( java.lang.Object o ) {
-        icse.nodecachinglinkedlist.LinkedListNode newNode = new icse.nodecachinglinkedlist.LinkedListNode(); //mutGenLimit 0
+    public boolean addFirst( java.lang.Object o, LinkedListNode newNode ) {
         newNode.value = o; //mutGenLimit 0
         icse.nodecachinglinkedlist.LinkedListNode insertBeforeNode = this.firstCachedNode.next; //mutGenLimit 1
         this.header.next = insertBeforeNode; //mutGenLimit 1
