3624800b1dac1f83c8ab2afcdcfa036e8ffd7221
Nazareno Aguirre <naguirre@gmail.com>
2016-04-11 15:35:45 -0300

    First case study originated from benchmark IntroClass, for Stryker.

---
+++ tests/ase2016/introclass/median/median_0cdfa335_003.java
@@ -0,0 +1,32 @@
+package ase2016.introclass.median;
+
+/**
+ * This class comes from benchmark introclass.
+ *
+ */
+
+public class median_0cdfa335_003 {
+
+    /*@
+    @ requires true;
+    @ ensures ((\result == a) || (\result == b) || (\result == c));
+    @ ensures ((a==b || a==c || b==c) ==> (\exists int n; (n == a) || (n == b) || (n == c); \result>n || \result<n));
+    @ ensures ((a!=b && a!=c && b!=c) ==> (\exists int n; (n == a) || (n == b) || (n == c); \result>n));
+    @ ensures ((a!=b && a!=c && b!=c) ==> (\exists int n; (n == a) || (n == b) || (n == c); \result<n));
+    @ signals (RuntimeException e) false;
+    @
+    @*/
+	public static int median(int a, int b, int c) {
+		int median; 
+        if ((a >= b && a <= c) || (a >= c && a <= b)) { //mutGenLimit 1
+            median = a;
+        }
+        if ((b >= a && b <= c) || (b >= c && b <= a)) { //mutGenLimit 1
+            median = b;
+        } else {
+            median = c;
+        }
+        return median;
+	}
+	
+}
---
+++ unittest/ase2016/introclass/median/StrykerIntroClassMedian_0cdfa335_0031Test.java
@@ -0,0 +1,39 @@
+package ase2016.introclass.median;
+
+import ar.edu.taco.regresion.CollectionTestBase;
+import ar.uba.dc.rfm.dynalloy.visualization.VizException;
+import mujava.api.Configuration;
+
+public class StrykerIntroClassMedian_0cdfa335_0031Test extends CollectionTestBase {
+
+	@Override
+	protected String getClassToCheck() {
+		return "ase2016.introclass.median.median_0cdfa335_003";
+	}
+
+			
+	public void test_containsTest() throws VizException {
+		setConfigKeyRelevantClasses("ase2016.introclass.median.median_0cdfa335_003");
+		setConfigKeyRelevancyAnalysis(true);
+		setConfigKeyCheckNullDereference(true);
+		setConfigKeyUseJavaArithmetic(false);
+		setConfigKeyCheckArithmeticException(true);
+		setConfigKeyInferScope(true);
+		setConfigKeyObjectScope(0);
+		setConfigKeyIntBithwidth(4);
+        setConfigKeyLoopUnroll(4);
+		setConfigKeySkolemizeInstanceInvariant(true);
+		setConfigKeySkolemizeInstanceAbstraction(true);
+		setConfigKeyGenerateUnitTestCase(true);
+		setConfigKeyAttemptToCorrectBug(true);
+		setConfigKeyMaxStrykerMethodsPerFile(1);
+		setConfigKeyRemoveQuantifiers(true);
+		setConfigKeyUseJavaSBP(true);
+		setConfigKeyUseTightUpperBounds(true);
+		setConfigKeyTypeScopes("ase2016.introclass.median.median_0cdfa335_003:1");
+		check(GENERIC_PROPERTIES,"median_0",true);
+	}
+
+
+
+}
