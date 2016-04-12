38bcc392446de3996b3480c083f2435ed34f2828
Nazareno Aguirre <naguirre@gmail.com>
2016-04-12 16:51:32 -0300

    Merged example.

--- tests/ase2016/introclass/median/introclass_0cdfa335_003.java
+++ tests/ase2016/introclass/median/introclass_0cdfa335_003.java
@@ -1,18 +1,10 @@
 package ase2016.introclass.median;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> db057c2264b26ac930335f60f2a121838cf3f38f
 public class introclass_0cdfa335_003 {
 
     public introclass_0cdfa335_003() {
     }
-<<<<<<< HEAD
-
-=======
     
->>>>>>> db057c2264b26ac930335f60f2a121838cf3f38f
     /*@
     @ requires true;
     @ ensures ((\result == \old(a)) || (\result == \old(b)) || (\result == \old(c)));
@@ -23,20 +15,6 @@ public class introclass_0cdfa335_003 {
     @
     @*/
     public int median( int a, int b, int c ) {
-<<<<<<< HEAD
-        int median;
-        if (a >= b && a <= c || a >= c && a <= b) { //mutGenLimit 1
-            median = a; //mutGenLimit 1
-        }
-        if (b >= a && b <= c || b >= c && b <= a) { //mutGenLimit 1
-            median = b; //mutGenLimit 1
-        } else {
-            median = c; //mutGenLimit 1
-        }
-        return median; //mutGenLimit 1
-    }
-
-=======
         int theMedian;
         if (a >= b && a <= c || a >= c && a <= b) { //mutGenLimit 1
             theMedian = a; //mutGenLimit 1
@@ -49,5 +27,4 @@ public class introclass_0cdfa335_003 {
         return theMedian; //mutGenLimit 1
     }
 	
->>>>>>> db057c2264b26ac930335f60f2a121838cf3f38f
 }
