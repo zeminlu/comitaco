9d72ebb94cf984e607f5fd60dd94df16d285f228
gastonscilingo <gaston@dc.exa.unrc.edu.ar>
2017-02-08 16:57:58 -0300

    fixes introclass median specification

--- tests/ase2016/introclass/median/introclass_0cea42f9_003.java
+++ tests/ase2016/introclass/median/introclass_0cea42f9_003.java
@@ -8,21 +8,23 @@ public class introclass_0cea42f9_003 {
     /*@
     @ requires true;
     @ ensures ((\result == a) || (\result == b) || (\result == c));
-    @ ensures ((a!=b || a!=c) ==> ( ((a==b) ==> (\result == a)) && ((b==c) ==> (\result ==b))));
+    @ ensures ( (a == b) ==> ((\result == a) || (\result == b) ) );
+    @ ensures ( (b == c) ==> ((\result == b) || (\result == c) ) );
+    @ ensures ( (a == c) ==> ((\result == a) || (\result == c) ) );
     @ ensures ((a!=b && a!=c && b!=c) ==> (\exists int n; (n == a) || (n == b) || (n == c); \result>n));
     @ ensures ((a!=b && a!=c && b!=c) ==> (\exists int n; (n == a) || (n == b) || (n == c); \result<n));
     @ signals (RuntimeException e) false;
     @
     @*/
 	public int median(int a, int b, int c) {
-        if (((a > b) && (a < c)) || ((a < b) && (a > c))) { //mutGenLimit 1
+        if (((a > b) && (a < c)) || ((a < b) && (a > c))) { //mutGenLimit 3
         	return a;
-        } else if (((b > a) && (b < c)) || ((b < a) && (b > c))) { //mutGenLimit 1
+        } else if (((b > a) && (b < c)) || ((b < a) && (b > c))) {
         	return b;
-        } else if (((c > a) && (c < b)) || ((c < a) && (c > b))) { //mutGenLimit 1
+        } else if (((c > a) && (c < b)) || ((c < a) && (c > b))) {
         	return c;
         }
-        return 0;
+        return b; //mutGenLimit 1
 	}
 	
 }
