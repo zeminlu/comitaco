e49469e87294ee57a011a14b12b36df49182430a
gastonscilingo <gaston@dc.exa.unrc.edu.ar>
2017-02-13 18:01:41 -0300

    fixes median specification on introclass_1bf73a9c_003.java

--- tests/ase2016/introclass/median/introclass_1bf73a9c_003.java
+++ tests/ase2016/introclass/median/introclass_1bf73a9c_003.java
@@ -8,7 +8,9 @@ public class introclass_1bf73a9c_003 {
     /*@
     @ requires true;
     @ ensures ((\result == a) || (\result == b) || (\result == c));
-    @ ensures ((a!=b || a!=c) ==> ( ((a==b) ==> (\result == a)) && ((b==c) ==> (\result ==b))));
+    @ ensures ( (a == b) ==> ((\result == a) || (\result == b) ) );
+    @ ensures ( (b == c) ==> ((\result == b) || (\result == c) ) );
+    @ ensures ( (a == c) ==> ((\result == a) || (\result == c) ) );
     @ ensures ((a!=b && a!=c && b!=c) ==> (\exists int n; (n == a) || (n == b) || (n == c); \result>n));
     @ ensures ((a!=b && a!=c && b!=c) ==> (\exists int n; (n == a) || (n == b) || (n == c); \result<n));
     @ signals (RuntimeException e) false;
@@ -16,14 +18,14 @@ public class introclass_1bf73a9c_003 {
     @*/
 	public int median(int a, int b, int c) {
 		int result;
-        if ((a > b && a < c) || (a > c && a < b)) { //mutGenLimit 1
+        if ((a > b && a < c) || (a > c && a < b)) { //mutGenLimit 3
             result = a;
-        } else if ((b > a && b < c) || (b > c && b < a)) { //mutGenLimit 1
+        } else if ((b > a && b < c) || (b > c && b < a)) { 
             result = b;
         } else {
             result = c;
         }
-        return result;
+        return result; //mutGenLimit 1
 	}
 	
 }
