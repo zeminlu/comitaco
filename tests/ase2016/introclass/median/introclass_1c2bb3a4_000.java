a6c487e03337048dce9b638a4f83cc7dbf9c77cb
Gastón Scilingo <gaston@dc.exa.unrc.edu.ar>
2017-02-14 15:41:57 -0300

    fixes introclass median specification (introclass_1c2bb3a4_000)

--- tests/ase2016/introclass/median/introclass_1c2bb3a4_000.java
+++ tests/ase2016/introclass/median/introclass_1c2bb3a4_000.java
@@ -8,7 +8,9 @@ public class introclass_1c2bb3a4_000 {
     /*@
     @ requires true;
     @ ensures ((\result == a) || (\result == b) || (\result == c));
-    @ ensures ((a!=b || a!=c) ==> ( ((a==b) ==> (\result == a)) && ((b==c) ==> (\result ==b))));
+    @ ensures ( (a == b) ==> ((\result == a) || (\result == b) ) );
+    @ ensures ( (b == c) ==> ((\result == b) || (\result == c) ) );
+    @ ensures ( (a == c) ==> ((\result == a) || (\result == c) ) );
     @ ensures ((a!=b && a!=c && b!=c) ==> (\exists int n; (n == a) || (n == b) || (n == c); \result>n));
     @ ensures ((a!=b && a!=c && b!=c) ==> (\exists int n; (n == a) || (n == b) || (n == c); \result<n));
     @ signals (RuntimeException e) false;
