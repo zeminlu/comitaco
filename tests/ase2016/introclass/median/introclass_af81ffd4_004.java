30b8c523f11294f6ae3101ec2582e9a7eea8e754
Gastón Scilingo <gaston@dc.exa.unrc.edu.ar>
2017-03-01 23:08:42 -0300

    was fixed wrong specification in introclass_3b2376ab_003 and introclass_af81ffd4_004

--- tests/ase2016/introclass/median/introclass_af81ffd4_004.java
+++ tests/ase2016/introclass/median/introclass_af81ffd4_004.java
@@ -8,22 +8,24 @@ public class introclass_af81ffd4_004 {
     /*@
     @ requires true;
     @ ensures ((\result == \old(a)) || (\result == \old(b)) || (\result == \old(c)));
-    @ ensures ((\old(a)!=\old(b) || \old(a)!=\old(c)) ==> ( ((\old(a)==\old(b)) ==> (\result == \old(a))) && ((\old(b)==\old(c)) ==> (\result ==\old(b)))));
+	@ ensures ( (a == b) ==> ((\result == a) || (\result == b) ) );
+    @ ensures ( (b == c) ==> ((\result == b) || (\result == c) ) );
+    @ ensures ( (a == c) ==> ((\result == a) || (\result == c) ) );
     @ ensures ((\old(a)!=\old(b) && \old(a)!=\old(c) && \old(b)!=\old(c)) ==> (\exists int n; (n == \old(a)) || (n == \old(b)) || (n == \old(c)); \result>n));
     @ ensures ((\old(a)!=\old(b) && \old(a)!=\old(c) && \old(b)!=\old(c)) ==> (\exists int n; (n == \old(a)) || (n == \old(b)) || (n == \old(c)); \result<n));
     @ signals (RuntimeException e) false;
     @
     @*/
     public int median( int a, int b, int c ) {
-    	int median = (abs (a) + abs (b) + abs (c)) / 3; //mutGenLimit 1
-        int comp_fir = abs (a - median); //mutGenLimit 1
-        int comp_sec = abs (b - median); //mutGenLimit 1
-        int comp_thi = abs (c - median); //mutGenLimit 1
+    	int median = (abs (a) + abs (b) + abs (c)) / 3; 
+        int comp_fir = abs (a - median); 
+        int comp_sec = abs (b - median); 
+        int comp_thi = abs (c - median); 
         if (comp_fir < comp_sec && comp_fir < comp_thi) { //mutGenLimit 1
-            return a;
-        } else if (comp_sec < comp_fir && comp_sec < comp_thi) { //mutGenLimit 1
+            return a;  //mutGenLimit 1
+        } else if (comp_sec < comp_fir && comp_sec < comp_thi) {
             return b;
-        } else if (comp_thi < comp_fir && comp_thi < comp_sec) { //mutGenLimit 1
+        } else if (comp_thi < comp_fir && comp_thi < comp_sec) {
             return c;
         }
         return 0; //mutGenLimit 1
