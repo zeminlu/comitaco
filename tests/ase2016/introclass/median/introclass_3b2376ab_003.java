731790264828fd8dd2e346e247d9e00476c1cc16
Gastón Scilingo <gaston@dc.exa.unrc.edu.ar>
2017-03-01 23:08:42 -0300

    was fixed wrong specification in introclass_3b2376ab_003 and introclass_af81ffd4_004

--- tests/ase2016/introclass/median/introclass_3b2376ab_003.java
+++ tests/ase2016/introclass/median/introclass_3b2376ab_003.java
@@ -8,7 +8,9 @@ public class introclass_3b2376ab_003 {
     /*@
     @ requires true;
     @ ensures ((\result == a) || (\result == b) || (\result == c));
-    @ ensures ((a!=b || a!=c) ==> ( ((a==b) ==> (\result == a)) && ((b==c) ==> (\result ==b))));
+    @ ensures ( (a == b) ==> ((\result == a) || (\result == b) ) );
+    @ ensures ( (b == c) ==> ((\result == b) || (\result == c) ) );
+    @ ensures ( (a == c) ==> ((\result == a) || (\result == c) ) );
     @ ensures ((a!=b && a!=c && b!=c) ==> (\exists int n; (n == a) || (n == b) || (n == c); \result>n));
     @ ensures ((a!=b && a!=c && b!=c) ==> (\exists int n; (n == a) || (n == b) || (n == c); \result<n));
     @ signals (RuntimeException e) false;
@@ -16,23 +18,23 @@ public class introclass_3b2376ab_003 {
     @*/
 	public int median(int a, int b, int c) {
 		int small;
-        if (a < b) { //mutGenLimit 1
+        if (a < b) { 
             small = a;
-            if (small > c) { //mutGenLimit 1
+            if (small > c) {
                 return a;
-            } else if (c > b) { //mutGenLimit 1
+            } else if (c > b) {
                 return b;
             } else {
                 return c;
             }
         } else {
             small = b;
-            if (small > c) { //mutGenLimit 1
+            if (small > c) { 
                 return b;
             } else if (c > a) { //mutGenLimit 1
-                return c;
+                return c; //mutGenLimit 0
             } else {
-                return a;
+                return a; //mutGenLimit 0
             }
         }
 	}
