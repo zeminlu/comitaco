7e266353482a9c6ea28bd322a52a755019b41355
Marcelo Frias <mffrias@gmail.com>
2017-07-23 16:32:20 -0300

    Simmlified code to speed up analysis during testing

--- tests/ar/edu/itba/complex/Complex.java
+++ tests/ar/edu/itba/complex/Complex.java
@@ -1,11 +1,11 @@
 package ar.edu.itba.complex;
 
 public class Complex {
-	public static  Complex I = new Complex(0.0f, 1.0f);
-	public static  Complex NaN = new Complex(0.0f / 0.0f, 0.0f / 0.0f);
-	public static  Complex INF = new Complex(1.0f / 0.0f, 1.0f / 0.0f);
-	public static  Complex ONE = new Complex(1.0f, 0.0f);
-	public static  Complex ZERO = new Complex(0.0f, 0.0f);
+	public   Complex I = new Complex(0.0f, 1.0f);
+	public   Complex NaN = new Complex(0.0f / 0.0f, 0.0f / 0.0f);
+	public   Complex INF = new Complex(1.0f / 0.0f, 1.0f / 0.0f);
+	public   Complex ONE = new Complex(1.0f, 0.0f);
+	public   Complex ZERO = new Complex(0.0f, 0.0f);
 
 	public float imaginary;
 	public  float real;
@@ -31,24 +31,25 @@ public class Complex {
 		if (isNaN) {
 			return NaN; //mutGenLimit 1
 		}
+		return null;
 
-		if (real == 0.0f && imaginary == 0.0f) {
-			return NaN; //mutGenLimit 1
-		}
-
-		if (isInfinite) {
-			return ZERO; //mutGenLimit 1
-		}
-
-		if (abs(real) < abs(imaginary)) {
-			float q = real / imaginary;
-			float scale =  1.0f / (real * q + imaginary);
-			return createComplex(scale * q, 0.0f - scale);
-		} else {
-			float q = imaginary / real;
-			float scale = 1.0f / (imaginary * q + real);
-			return createComplex(scale, (0.0f - scale) * q);
-		}
+//		if (real == 0.0f && imaginary == 0.0f) {
+//			return NaN; //mutGenLimit 1
+//		}
+//
+//		if (isInfinite) {
+//			return ZERO; //mutGenLimit 1
+//		}
+//
+//		if (abs(real) < abs(imaginary)) {
+//			float q = real / imaginary;
+//			float scale =  1.0f / (real * q + imaginary);
+//			return createComplex(scale * q, 0.0f - scale);
+//		} else {
+//			float q = imaginary / real;
+//			float scale = 1.0f / (imaginary * q + real);
+//			return createComplex(scale, (0.0f - scale) * q);
+//		}
 	}
 
 
